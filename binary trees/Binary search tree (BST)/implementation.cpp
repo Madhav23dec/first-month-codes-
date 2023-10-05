43e6f476926caee87ddb6b69fbe8ad228f589957
madhav <madhav23dec@gmail.com>
2023-10-06 00:17:49 +0530

    first month codes

---
+++ binary trees/Binary search tree (BST)/implementation.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+node()
+{
+public:
+  int data ;
+  node* right;
+  node* left;
+
+  node( int d)
+  {
+    this->data= d;
+    left= NULL;
+    right= NULL;
+  }
+}
+
+node* buildBST(node* root)
+{
+  int data ;
+  cin>>data;
+  if(data==NULL)
+    root=NULL;
+
+
+}
+
+
+
+int main()
